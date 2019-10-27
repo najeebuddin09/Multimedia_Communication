206643a2c37abe332157cb89b0fd39d6807cfa22
najeebuddin09 <najeebuddin09@users.noreply.github.com>
2019-10-27 17:01:56 +0500

    created exe files

--- internet all media transmission over socket/serverMedia.py
+++ internet all media transmission over socket/serverMedia.py
@@ -1,45 +1,93 @@
 # this receive video and audio from one client and then broadcast it to the other connected clients
+
+#
+# Imports
+#
+
+# for sockets
 from socket import socket, AF_INET, SOCK_STREAM
+
+# for multithreading
 from threading import Thread
+
+# to convert the received data from strings to orignal format
 import struct
 
+#
+# Globals
+#
+
+# Host address i.e the address the server is running on
 HOST = input("Enter Host IP\n")
+
+# Port for video transmission
 PORT_VIDEO = 3000
+
+# Port for audio transmission
 PORT_AUDIO = 4000
+
+#
 lnF = 640*480*3
+
+#
 CHUNK = 1024
+
+# audio data to be received from clients
 BufferSize = 4096
+
+# for storing the addresses of clients for audio tansmission
 addressesAudio = {}
+
+#
 addresses = {}
+
+#
 threads = {}
 
+#
+# Function Definitions
+#
+
+#
 def ConnectionsVideo():
     while True:
         try:
+            # accept connection from client for video transmission
             clientVideo, addr = serverVideo.accept()
             print("{} is connected!!".format(addr))
+
+            # store the address of clients
             addresses[clientVideo] = addr
             if len(addresses) > 1:
                 for sockets in addresses:
                     if sockets not in threads:
                         threads[sockets] = True
+                        # inform the client to start transmission
                         sockets.send(("start").encode())
+                        # create thread for video transmission
                         Thread(target=ClientConnectionVideo, args=(sockets, )).start()
             else:
                 continue
         except:
             continue
 
+# Accept a connection from client for audio and then create a thread for audio broadcasting of that client to other clients
 def ConnectionsSound():
     while True:
         try:
+            # accept connection
             clientAudio, addr = serverAudio.accept()
             print("{} is connected!!".format(addr))
+
+            # storing the address of each client
             addressesAudio[clientAudio] = addr
+
+            # Create a thread to start sound transmission
             Thread(target=ClientConnectionSound, args=(clientAudio, )).start()
         except:
             continue
 
+# receive data from clients and then send it to other clients
 def ClientConnectionVideo(clientVideo):
     while True:
         try:
@@ -49,6 +97,7 @@ def ClientConnectionVideo(clientVideo):
         except:
             continue
 
+# receive audio from client and then broadcast it to other clients
 def ClientConnectionSound(clientAudio):
     while True:
         try:
@@ -57,6 +106,7 @@ def ClientConnectionSound(clientAudio):
         except:
             continue
 
+# receive function for receiving video data
 def recvall(clientVideo, BufferSize):
         databytes = b''
         i = 0
@@ -84,8 +134,10 @@ def broadcastVideo(clientSocket, data_to_be_sent):
         if clientVideo != clientSocket:
             clientVideo.sendall(data_to_be_sent)
 
+# broadcast audio to other clients
 def broadcastSound(clientSocket, data_to_be_sent):
     for clientAudio in addressesAudio:
+        # send to all clients except the one who send it
         if clientAudio != clientSocket:
             clientAudio.sendall(data_to_be_sent)
 
@@ -101,15 +153,28 @@ try:
 except OSError:
     print("Server Busy")
 
+#
+# "__main__"
+#
+
+# listen for Audio connections from clients. 2 = backlog
 serverAudio.listen(2)
 print("Waiting for connection..")
+
+# Creating a thread for sound transmission
 AcceptThreadAudio = Thread(target=ConnectionsSound)
 AcceptThreadAudio.start()
 
-
+# listening for video connection from clients. 2 = backlog
 serverVideo.listen(2)
 print("Waiting for connection..")
+
+# Creating a thread for video transmission
 AcceptThreadVideo = Thread(target=ConnectionsVideo)
 AcceptThreadVideo.start()
+
+# waiting for video thread to complete
 AcceptThreadVideo.join()
+
+# closing socket for video transmission
 serverVideo.close()
